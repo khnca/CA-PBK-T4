af0e04dd01aee7aca28e901f10ca7672d6ddc53c
khansa <nadiakhalisahkhansa16@gmail.com>
2024-05-15 19:23:53 +0700

    first commit

---
+++ my-vue-app/src/components/childtwo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="child">
+      <h3>Child Two</h3>
+      <button @click="sendData">Klik Tombol Ini</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'childtwo',
+    emits: ['data-from-child'],
+    setup(props, { emit }) {
+      const sendData = () => {
+        emit('data-from-child', 'Data from child two');
+      };
+  
+      return {
+        sendData,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .child {
+    border: 1px solid #d21b45;
+    padding: 10px;
+    margin: 10px;
+    text-align: center;
+  }
+  button {
+    background-color: #ec114f;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: #e4247e;
+  }
+  </style>
+  
